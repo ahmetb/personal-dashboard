512746fdab6b542947dbd5a04559d358e158e906
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2013-08-31 22:02:00 -0700

    update jawbone erroneus log stmt

--- tasks/jawboneup.py
+++ tasks/jawboneup.py
@@ -1,6 +1,10 @@
 # coding: utf-8
 from . import requires, today_utc
 import requests
+from datetime import timedelta
+
+
+DAYS_BACK = 2
 
 @requires('jawboneup.access_token')
 def sleeps(gauge_factory, config, logger):
@@ -11,20 +15,21 @@ def sleeps(gauge_factory, config, logger):
     r = requests.get('https://jawbone.com/nudge/api/users/@me/sleeps',
                      headers=headers)
 
-    day = today_utc()
-    resp = r.json()
-    sleeps = resp['data']['items']
-    today = day.date()
-    today_fmt = int(today.strftime('%Y%m%d'))
-    today_sleeps = filter(lambda s: s['date'] == today_fmt, sleeps)
-    
-    duration = sum([s['details']['duration'] - s['details']['awake']
-                   for s in today_sleeps]) / 60.0 # in minutes
-    if duration == 0:
-        logger.info('Sleeps not found today, not saving.')
-    else:
-        gauge.save(day, duration)
-        logger.info('Saved {0} min sleep for {1}'.format(duration, day))
+    for i in range(DAYS_BACK):
+        day = today_utc() - timedelta(days=i)
+        resp = r.json()
+        sleeps = resp['data']['items']
+        day_date = day.date()
+        today_fmt = int(day_date.strftime('%Y%m%d'))
+        today_sleeps = filter(lambda s: s['date'] == today_fmt, sleeps)
+        
+        duration = sum([s['details']['duration'] - s['details']['awake']
+                       for s in today_sleeps]) / 60.0 # in minutes
+        if duration == 0:
+            logger.info('Sleeps not found on {0}, not saving.'.format(day_date))
+        else:
+            gauge.save(day, duration)
+            logger.info('Saved {0}min sleep on {1}'.format(duration, day_date))
 
 
 @requires('jawboneup.access_token')
@@ -35,16 +40,18 @@ def steps(gauge_factory, config, logger):
     headers = {'Authorization' : 'Bearer {0}'.format(access_token)}
     r = requests.get('https://jawbone.com/nudge/api/users/@me/moves',
                      headers=headers)
-    day = today_utc()
-    resp = r.json()
-    moves = resp['data']['items']
-    today = day.date()
-    today_fmt = int(today.strftime('%Y%m%d'))
-    today_moves = filter(lambda m: m['date'] == today_fmt, moves)
-    steps = sum([m['details']['steps'] for m in today_moves])
 
-    if steps == 0:
-        logger.info('Steps not found for today yet, not saving.')
-    else:
-        gauge.save(day, steps)
-        logger.info('Saved {0} steps for {1}'.format(steps, day))
+    for i in range(DAYS_BACK):
+        day = today_utc() - timedelta(days=i)
+        resp = r.json()
+        moves = resp['data']['items']
+        day_date = day.date()
+        today_fmt = int(day_date.strftime('%Y%m%d'))
+        today_moves = filter(lambda m: m['date'] == today_fmt, moves)
+        steps = sum([m['details']['steps'] for m in today_moves])
+
+        if steps == 0:
+            logger.info('Steps not found on {0}, not saving.'.format(day_date))
+        else:
+            gauge.save(day, steps)
+            logger.info('Saved {0} steps on {1}'.format(steps, day_date))
